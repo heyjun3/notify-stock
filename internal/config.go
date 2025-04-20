8f114299b1e22c0abeecd742245cece8e030a8ce
heyjun3 <t3k2g5kako@yahoo.co.jp>
2025-04-20 13:11:05 +0900

    fix config

--- internal/config.go
+++ internal/config.go
@@ -13,15 +13,29 @@ func init() {
 	if err != nil {
 		logger.Info(err.Error())
 	}
-	Cfg.FROM = os.Getenv("FROM")
-	Cfg.TO = os.Getenv("TO")
-	Cfg.DBDSN = "postgres://postgres:postgres@localhost:5555/notify-stock?sslmode=disable"
-	Cfg.MailToken = os.Getenv("MAIL_TOKEN")
+	from, ok := os.LookupEnv("FROM")
+	if !ok {
+		panic("FROM is not set")
+	}
+	to, ok := os.LookupEnv("TO")
+	if !ok {
+		panic("TO is not set")
+	}
+	mailToken, ok := os.LookupEnv("MAIL_TOKEN")
+	if !ok {
+		panic("MAIL_TOKEN is not set")
+	}
+	Cfg = Config{
+		FROM:      from,
+		TO:        to,
+		DBDSN:     "postgres://postgres:postgres@localhost:5555/notify-stock?sslmode=disable",
+		MailToken: mailToken,
+	}
 }
 
 type Config struct {
-	FROM          string
-	TO            string
-	DBDSN         string
+	FROM      string
+	TO        string
+	DBDSN     string
 	MailToken string
 }

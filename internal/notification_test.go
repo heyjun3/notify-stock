a41709518c0dc10f497ec3d288ed4fe4158b3826
heyjun3 <t3k2g5kako@yahoo.co.jp>
2025-04-22 00:28:12 +0900

    update

--- internal/notification_test.go
+++ internal/notification_test.go
@@ -7,26 +7,9 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
-	"github.com/uptrace/bun"
 
 	notify "github.com/heyjun3/notify-stock/internal"
 )
-
-func openDB(t *testing.T) *bun.DB {
-	t.Helper()
-	dsn := "postgres://postgres:postgres@localhost:5555/notify-stock-test?sslmode=disable"
-	db := notify.NewDB(dsn)
-	for _, table := range []any{
-		(*notify.Stock)(nil),
-		(*notify.Notification)(nil),
-	} {
-		db.NewDelete().Model(table).Where("1 = 1").Exec(context.Background())
-	}
-	t.Cleanup(func() {
-		db.Close()
-	})
-	return db
-}
 
 func NoError[T any](t *T, err error) T {
 	return *t
---
+++ internal/utils_test.go
@@ -0,0 +1,26 @@
+package notifystock_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/uptrace/bun"
+
+	notify "github.com/heyjun3/notify-stock/internal"
+)
+
+func openDB(t *testing.T) *bun.DB {
+	t.Helper()
+	dsn := "postgres://postgres:postgres@localhost:5555/notify-stock-test?sslmode=disable"
+	db := notify.NewDB(dsn)
+	for _, table := range []any{
+		(*notify.Stock)(nil),
+		(*notify.Notification)(nil),
+	} {
+		db.NewDelete().Model(table).Where("1 = 1").Exec(context.Background())
+	}
+	t.Cleanup(func() {
+		db.Close()
+	})
+	return db
+}
